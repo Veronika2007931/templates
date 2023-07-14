f7e04d741b81d3774ea81b9c44100a2d0a2d9c18
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2023-07-14 18:09:39 +0300

    add

---
+++ src/arr.js
@@ -0,0 +1,23 @@
+[
+    { object: Chair,
+     color: white,
+     material: plastic
+    },
+    { object: Table,
+     color: brown,
+     material: wood
+    },
+    { object: Plates,
+     color: cream,
+     material: ceramics
+    },
+    { object: Vase,
+     color: green,
+     material: ceramics
+    },
+    { object: Cup,
+     color: white,
+     material: glass
+    }
+ 
+ ]
--- src/hw-7.js
+++ src/hw-7.js
@@ -1,30 +1,8 @@
+import arr from './arr.js'
 import listItems from "../template/list.handlebars"
 
-const arr = [
-   { object: Chair,
-    color: white,
-    material: plastic
-   },
-   { object: Table,
-    color: brown,
-    material: wood
-   },
-   { object: Plates,
-    color: cream,
-    material: ceramics
-   },
-   { object: Vase,
-    color: green,
-    material: ceramics
-   },
-   { object: Cup,
-    color: white,
-    material: glass
-   }
 
-]
-
-const gallery = document.querySelector('.gallery')
+const gallery = document.querySelector('.info')
 const markup = listItems(arr)
 gallery.innerHTML = markup
 
